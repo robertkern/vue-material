7b389ab67241382c84d838f3cb21ff6e7dd44776
Robert Kern <me@robertkern.com>
2017-08-11 07:45:24 +1200

    Add basic router-link type support to mdc-button

--- src/components/button/mixins/button.js
+++ src/components/button/mixins/button.js
@@ -46,6 +46,21 @@ export default {
       type: Boolean,
       required: false,
       default: false
+    },
+    to: {
+      type: [String, Object], // "to" is not part of MDC spec, used for router-link type func
+      required: false,
+      default: ''
+    },
+    replace: {
+      type: Boolean, // "replace" is not part of MDC spec, used for router-link type func
+      required: false,
+      default: false
+    },
+    append: {
+      type: Boolean, // "append" is not part of MDC spec, used for router-link type func
+      required: false,
+      default: false
     }
   },
   mounted () {
@@ -64,6 +79,14 @@ export default {
   },
   render (createElement) {
     let vm = this
+
+    if (vm.$router) {
+      // Handle router links
+      var router = vm.$router
+      var current = vm.$route
+      var { location, href } = router.resolve(vm.to, current, vm.append)
+    }
+
     let data = {
       class: {
         'mdc-button': true,
@@ -79,12 +102,48 @@ export default {
         disabled: vm.disabled
       },
       on: {
-        click () {
+        ...vm.$listeners,
+        click (e) {
+          if (router && guardRouteClickEvent(e)) {
+            if (vm.replace) {
+              router.replace(location)
+            } else {
+              router.push(location)
+            }
+          }
+
           vm.$emit('click')
         }
       }
     }
 
+    if (vm.tag === 'a' && href && href.length > 0) {
+      // Set the href with the to route value
+      data.attrs = {
+        href
+      }
+    }
+
     return createElement(vm.tag, data, vm.$slots.default)
   }
+}
+
+// Lifted directly from https://github.com/vuejs/vue-router/blob/dev/src/components/link.js
+function guardRouteClickEvent (e) {
+  // don't redirect with control keys
+  if (e.metaKey || e.altKey || e.ctrlKey || e.shiftKey) return
+  // don't redirect when preventDefault called
+  if (e.defaultPrevented) return
+  // don't redirect on right click
+  if (e.button !== undefined && e.button !== 0) return
+  // don't redirect if `target="_blank"`
+  if (e.currentTarget && e.currentTarget.getAttribute) {
+    const target = e.currentTarget.getAttribute('target')
+    if (/\b_blank\b/i.test(target)) return
+  }
+  // this may be a Weex event which doesn't have this method
+  if (e.preventDefault) {
+    e.preventDefault()
+  }
+  return true
 }
