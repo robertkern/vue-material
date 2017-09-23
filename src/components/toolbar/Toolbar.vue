64ae684613fdea9298ba6f9fb2860502bb04b187
Robert Kern <me@robertkern.com>
2017-09-23 20:34:03 +1200

    Various updates and fixes to toolbar

--- src/components/toolbar/Toolbar.vue
+++ src/components/toolbar/Toolbar.vue
@@ -13,12 +13,43 @@ export default {
       type: Boolean,
       required: false,
       default: false
+    },
+    waterfall: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    fixedLastRowOnly: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    flexible: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    flexibleDefaultBehavior: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
   data () {
     return {
       classes: {},
       foundation: null
+    }
+  },
+  computed: {
+    firstRowElement () {
+      return this.$el.querySelector(MDCToolbarFoundation.strings.FIRST_ROW_SELECTOR)
+    },
+    titleElement () {
+      return this.$el.querySelector(MDCToolbarFoundation.strings.TITLE_SELECTOR)
+    },
+    fixedAdjustElement () {
+      return this.getSiblingWithClass('mdc-toolbar-fixed-adjust')
     }
   },
   mounted () {
@@ -35,16 +66,16 @@ export default {
         vm.$delete(vm.classes, className)
       },
       registerScrollHandler (handler) {
-        vm.$el.addEventListener('scroll', handler, util.applyPassive())
+        window.addEventListener('scroll', handler, util.applyPassive())
       },
       deregisterScrollHandler (handler) {
-        vm.$el.removeEventListener('scroll', handler, util.applyPassive())
+        window.removeEventListener('scroll', handler, util.applyPassive())
       },
       registerResizeHandler (handler) {
-        vm.$el.addEventListener('resize', handler, util.applyPassive())
+        window.addEventListener('resize', handler)
       },
       deregisterResizeHandler (handler) {
-        vm.$el.removeEventListener('resize', handler, util.applyPassive())
+        window.removeEventListener('resize', handler)
       },
       getViewportWidth () {
         return window.innerWidth
@@ -53,22 +84,22 @@ export default {
         return window.pageYOffset
       },
       getOffsetHeight () {
-        return vm.$refs.root.offsetHeight
+        return vm.$el.offsetHeight
       },
       getFirstRowElementOffsetHeight () {
-        // vm.firstRowElement_.offsetHeight
+        return vm.firstRowElement.offsetHeight
       },
       notifyChange (evtData) {
         vm.$emit(MDCToolbarFoundation.strings.CHANGE_EVENT, evtData)
       },
       setStyle (property, value) {
-        vm.$refs.root.style.setProperty(property, value)
+        vm.$el.style.setProperty(property, value)
       },
       setStyleForTitleElement (property, value) {
-        // vm.titleElement_.style.setProperty(property, value)
+        vm.titleElement.style.setProperty(property, value)
       },
       setStyleForFlexibleRowElement (property, value) {
-        // vm.firstRowElement_.style.setProperty(property, value)
+        vm.firstRowElement.style.setProperty(property, value)
       },
       setStyleForFixedAdjustElement (property, value) {
         if (vm.fixedAdjustElement) {
@@ -79,13 +110,26 @@ export default {
 
     vm.foundation.init()
   },
+  methods: {
+    getSiblingWithClass (className) {
+      if (this.$el.nextElementSibling) {
+        return (this.$el.nextElementSibling.className.indexOf(className) >= 0) ? this.$el.nextElementSibling : null
+      }
+
+      return null
+    }
+  },
   render (createElement) {
     let data = {
       class: {
         'mdc-toolbar': true,
-        'mdc-toolbar--fixed': this.fixed
-      },
-      ref: 'root'
+        'mdc-toolbar--fixed': this.fixed,
+        'mdc-toolbar--waterfall': this.waterfall,
+        'mdc-toolbar--fixed-lastrow-only': this.fixedLastRowOnly,
+        'mdc-toolbar--flexible': this.flexible,
+        'mdc-toolbar--flexible-default-behavior': this.flexibleDefaultBehavior,
+        ...this.classes
+      }
     }
 
     return createElement(this.tag, data, this.$slots.default)
